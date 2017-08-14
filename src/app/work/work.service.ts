7c6e7ac3f11a4ef27992b9df04e7a34afecf249a
mst-t <u456903j@gmail.com>
2017-08-14 00:39:36 -0400

    make it better

--- src/app/work/work.service.ts
+++ src/app/work/work.service.ts
@@ -21,49 +21,48 @@ export class WorkService {
       "https://github.com/mst-t/Favorite-Article-Clipper",
       "https://sheltered-savannah-96980.herokuapp.com/",
       ["AngularJs(v-1.6)", "Express", "Node.js", "ECMAScript 6", "Bootstrap", "AYLIEN API"]),
-     new Work(
-        "Blackboard",
-        `This is a simple TODO application powerd by MEAN stack. You can get, create, update, and delete messages using REST APIs.
-         The web application is hosted on Heroku and the database is running on mLab. Click on the choke to enable or disable edit mode.
-        `,
-        "/assets/images/blackboard.png",
-        "https://github.com/mst-t/blackboard",
-        "https://sheltered-retreat-34258.herokuapp.com/",
-        ["Angular4", "MongoDB", "Express", "Node", "Promise", "Angular CLI", "HTML5", "CSS3", "Bootstrap"]),
-      new Work(
-        "Gallery Website",
-        `This is a single page application created using Angular(Angular4). Authentication function is implemented using Firebase.
-         I am currently using this application as my portfolio, so the function is restricted.
-         After signing up or logging in, the user can add, change, or delete information temporarily.
-         Changed data will not be saved. When you press Reset or reload the webpage, the changed data will return to the initial state.
-        `,
-        "/assets/images/gallery.png",
-        "https://github.com/mst-t/gallery-website",
-        "https://mst-t.github.io",
-        ["Angular4", "Firebase", "Auth", "Promise", "Angular CLI", "HTML5", "CSS3", "Bootstrap"]),
-      new Work(
-        "Portfolio Design",
-        `This is a web page created using Bootstrap, based on grid layout.
-         I realized the responsive portfolio website with a concise and easy-to-see design.
-         Also, I have implemented some animations using jQuery. The pictures show my favorite things.
-        `,
-        "./assets/images/portfolio.png",
-        "https://github.com/mst-t/portfolio-design",
-        "https://psychiatrist-goat-68182.netlify.com/",
-        ["HTML5", "CSS3", "Bootstrap", "jQuery", "Animation", "Grid Layout", "Bower"]),
-      new Work(
-        "Montreal Island Map",
-        `This is a Udacity project created to learn the MVVM pattern and Knockout.js
-         which is one of the JavaScript frameworks useful for creating single-page applications.
-         Icons and list items are displayed using the Google Map API and Knockout functions.
-         By clicking on them, the information on the location is displayed on the map using the Wikipedia API.
-         Also, I adopted the flat design, changed the design of the map, and selected icons suitable for it.
-        `,
-        "./assets/images/neighborhoodMap.png",
-        "https://github.com/mst-t/neighborhood-map",
-        "http://driver-polecat-44588.netlify.com",
-        ["KnockoutJs", "Google Map API", "Wikipedia API", "MVVM Pattern", "Ajax", "Google Map Customization", "Flat Design"]
-      ),
+    new Work(
+      "Blackboard",
+      `This is a simple TODO application powerd by MEAN stack. You can get, create, update, and delete messages using REST APIs.
+       The web application is hosted on Heroku and the database is running on mLab. Click on the choke to enable or disable edit mode.
+      `,
+      "/assets/images/blackboard.png",
+      "https://github.com/mst-t/blackboard",
+      "https://sheltered-retreat-34258.herokuapp.com/",
+      ["Angular4", "MongoDB", "Express", "Node", "Promise", "Angular CLI", "HTML5", "CSS3", "Bootstrap"]),
+    new Work(
+      "Gallery Website",
+      `This is a single page application created using Angular(Angular4). Authentication function is implemented using Firebase.
+       I am currently using this application as my portfolio, so the function is restricted.
+       After signing up or logging in, the user can add, change, or delete information temporarily.
+       Changed data will not be saved. When you press Reset or reload the webpage, the changed data will return to the initial state.
+      `,
+      "/assets/images/gallery.png",
+      "https://github.com/mst-t/gallery-website",
+      "https://mst-t.github.io",
+      ["Angular4", "Firebase", "Auth", "Promise", "Angular CLI", "HTML5", "CSS3", "Bootstrap"]),
+    new Work(
+      "Portfolio Design",
+      `This is a web page created using Bootstrap, based on grid layout.
+       I realized the responsive portfolio website with a concise and easy-to-see design.
+       Also, I have implemented some animations using jQuery. The pictures show my favorite things.
+      `,
+      "./assets/images/portfolio.png",
+      "https://github.com/mst-t/portfolio-design",
+      "https://psychiatrist-goat-68182.netlify.com/",
+      ["HTML5", "CSS3", "Bootstrap", "jQuery", "Animation", "Grid Layout", "Bower"]),
+    new Work(
+      "Montreal Island Map",
+      `This is a Udacity project created to learn the MVVM pattern and Knockout.js
+       which is one of the JavaScript frameworks useful for creating single-page applications.
+       Icons and list items are displayed using the Google Map API and Knockout functions.
+       By clicking on them, the information on the location is displayed on the map using the Wikipedia API.
+       Also, I adopted the flat design, changed the design of the map, and selected icons suitable for it.
+      `,
+      "./assets/images/neighborhoodMap.png",
+      "https://github.com/mst-t/neighborhood-map",
+      "http://driver-polecat-44588.netlify.com",
+      ["KnockoutJs", "Google Map API", "Wikipedia API", "MVVM Pattern", "Ajax", "Google Map Customization", "Flat Design"]),
   ];
 
   private defaultWorks: Array<Work> = this.works.concat();
